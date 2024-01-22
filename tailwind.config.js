640b9313be76b530d4b13c5994624e7212644b4a
Théo Larsimont <larsimontheo@gmail.com>
2024-01-22 10:59:28 +0100

    finition main css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'html': '#FF8817',
-        'css': '#187FCC',
-        'js': '#F0B209',
-        'sql': '#C800FF'
+        'html': 'rgba(255,136,23,0.78)',
+        'css': 'rgba(24,127,204,0.78)',
+        'js': 'rgba(240,178,9,0.78)',
+        'sql': 'rgba(200,0,255,0.78)',
+        'java': 'rgba(173,40,40,0.78)',
+        'title': '#3E675C',
+        'side-bar': '#63A594',
+        'main': '#e7e7e7'
 
       }
     },
