511b9b63a8c7f2b517e7f2885c2711ce34a79724
Théo Larsimont <theolarsimont@proton.me>
2024-09-21 10:55:25 +0200

    Changement photo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         'css': 'rgba(24,127,204,0.78)',
         'js': 'rgba(240,178,9,0.78)',
         'sql': 'rgba(200,0,255,0.78)',
-        'java': 'rgba(173,40,40,0.78)',
+        'java': 'rgba(166,213,31,0.78)',
+        'angular': 'rgba(173,40,40,0.78)',
         'title': '#3E675C',
         'side-bar': '#63A594',
         'main': '#e7e7e7'
